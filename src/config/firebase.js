b4c9b258cba52bf40e7420469669ef5ee5db529f
Adidya Abimanyu <114373205+AdidyaAbimanyu@users.noreply.github.com>
2025-01-20 14:17:20 +0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,18 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: "AIzaSyBk2qi9MOgRGxP3WrmaQEiv9gTsEyobRi8",
+  authDomain: "universities-b7318.firebaseapp.com",
+  databaseURL: "https://universities-b7318.firebaseio.com",
+  projectId: "universities-b7318",
+  storageBucket: "universities-b7318.firebasestorage.app",
+  messagingSenderId: "664588779907",
+  appId: "1:664588779907:web:9dc6f1fa45dcbedbcff6b6",
+  measurementId: "G-FYY5MTGQ0F"
 };
 
 const app = initializeApp(firebaseConfig);
